e004d4b259005b841160f23686227d45f0e6e270
Madhvesh77 <madhvesh77@gmail.com>
2024-07-28 15:00:26 +0530

    Make the opening image fade away as scroll

--- storytime/src/app/page.tsx
+++ storytime/src/app/page.tsx
@@ -19,32 +19,38 @@ export default function Home() {
     requestAnimationFrame(raf)
   }, [])
 
-    const container = useRef(null);
+  const container = useRef(null);
   // const { scrollYProgress } = useScroll();
-  
+
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ['start start', 'end end']
   })
 
-const scale = useTransform(scrollYProgress, [0, 1], [1, 1.5]);
-const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 1.3]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+  const newDivOpacity = useTransform(scrollYProgress, [0.5, 1], [0, 1]);
 
   return (
     <main className=" mb-[100vh]">
       <div ref={container} className="flex justify-center font-semibold text-9xl h-[200vh]">
-        <div className="sticky top-0 h-[120vh] bg-orange-300 overflow-hidden">
+        <div className="sticky top-0 h-[100vh] bg-black overflow-hidden">
           {/* <div className=" absolute top-0"> */}
-            <motion.div style={{scale}} className="relative h-[100vh]">
-              <Image
-                src={opening}
-                alt="opening-image"
-                className="h-full w-full object-cover"
-              />
-              <motion.div className="absolute inset-0 flex justify-center items-center" style={{ opacity }}> 
+          <motion.div style={{ scale }} className="relative h-[100vh]">
+            <motion.div style={{ opacity }}>
+            <Image
+              src={opening}
+              alt="opening-image"
+              className="h-full w-full object-cover"
+            />
+            </motion.div>
+            <motion.div className="absolute inset-0 flex justify-center items-center" style={{ opacity }}>
               <h1 className="text-white">Hello</h1>
             </motion.div>
+            <motion.div className="absolute inset-0 flex justify-center items-center" style={{ opacity: newDivOpacity }}>
+              <h1 className="text-white">Welcome</h1>
             </motion.div>
+          </motion.div>
           {/* </div> */}
         </div>
       </div>
