b1b011cac95b566a4a25cd6596b6d7e87fb8a02f
Madhvesh77 <madhvesh77@gmail.com>
2024-08-11 23:05:04 +0530

    extract ch1 scene2 as separate component

--- storytime/src/app/page.tsx
+++ storytime/src/app/page.tsx
@@ -1,27 +1,19 @@
 'use client'
-import { motion, useScroll, useTransform } from "framer-motion";
 import TitleCardAndPlot from "@/components/TitleCardAndPlot";
 import Image from "next/image";
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
-import { useRef, useState } from "react";
+import { useState } from "react";
 import map from "../../public/Map.png";
-import war from "../../public/War.png";
 import opening from "../../public/Opening.png";
 import Scene1 from "@/components/Chapter1/Scene1";
+import Scene2 from "@/components/Chapter1/Scene2";
 
 export default function Home() {
   const plot = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi rem sed commodi cumque accusamus molestiae, sequi ullam accusantium modi, facere tenetur fuga eos cum nisi maiores, ad saepe aspernatur! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, aliquam. Libero excepturi eaque, eius velit est repellendus fugiat dolores voluptatem quia, adipisci, officiis ad cupiditate blanditiis nostrum aut delectus illum?"
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  const ref = useRef(null);
-  const warRef = useRef(null);
-  const { scrollYProgress: warScrollYProgress } = useScroll({ target: warRef })
-  const { scrollYProgress } = useScroll({ target: ref });
-  const zoom = useTransform(scrollYProgress, [0, 0.2], [1, 4]);
-  const opacity = useTransform(scrollYProgress, [0, 0.2], [1, 0]);
-  const revOpacity = useTransform(scrollYProgress, [0.1, 0.3], [0, 1]);
-  const moveLeft = useTransform(scrollYProgress, [0, 0.5], [0, -2000])
+  
 
   const handleClick = (() => {
     setIsDialogOpen(true);
@@ -48,49 +40,7 @@ export default function Home() {
       </Dialog>
       <TitleCardAndPlot image={opening} paragraph={plot} />
       <Scene1 />
-      <div ref={ref} className="h-[400vh] bg-black mt-28">
-        <div className="sticky top-0 h-[200vh]">
-          <motion.div className="relative">
-            <motion.div style={{ scale: zoom, opacity, transformOrigin: '70% 70%' }}>
-              <Image
-                src={map}
-                alt="map"
-                className="w-full h-full" />
-            </motion.div>
-            <motion.div
-              ref={warRef}
-              className="absolute bg-red-400 inset-0 overflow-hidden "
-              style={{ scale: revOpacity }}
-            >
-              <div className="relative w-[200vh] h-full">
-                <motion.div
-                  className="absolute flex flex-row w-[200vh] h-full bg-red-400"
-                  style={{ x: moveLeft }}
-                >
-                  <Image
-                    src={war}
-                    alt="war"
-                    className="w-full h-[95%]"
-                  />
-                  <Image
-                    src={war}
-                    alt="war"
-                    className="w-full h-[95%]"
-                  />
-                  <Image
-                    src={war}
-                    alt="war"
-                    className="w-full h-[95%]"
-                  />
-                </motion.div>
-                <p className="absolute font-edu-hand text-lg flex justify-center w-[90vh] inset-[50vh]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta esse aliquid vitae quam, ex, ipsum ratione, nostrum illo voluptatibus commodi saepe sint mollitia vero tenetur ut quod animi tempore.</p>
-              </div>
-            </motion.div>
-
-
-          </motion.div>
-        </div>
-      </div>
+      <Scene2 />
     </main>
   )
 }
