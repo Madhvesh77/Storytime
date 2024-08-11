f08670deae0475cb1bb19cf9fb2670620d6991ce
Madhvesh77 <madhvesh77@gmail.com>
2024-08-11 23:00:20 +0530

    add map zoom in and war image

--- storytime/src/app/page.tsx
+++ storytime/src/app/page.tsx
@@ -15,10 +15,13 @@ export default function Home() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const ref = useRef(null);
+  const warRef = useRef(null);
+  const { scrollYProgress: warScrollYProgress } = useScroll({ target: warRef })
   const { scrollYProgress } = useScroll({ target: ref });
   const zoom = useTransform(scrollYProgress, [0, 0.2], [1, 4]);
   const opacity = useTransform(scrollYProgress, [0, 0.2], [1, 0]);
-  const revOpacity = useTransform(scrollYProgress, [0, 0.2], [0, 1]);
+  const revOpacity = useTransform(scrollYProgress, [0.1, 0.3], [0, 1]);
+  const moveLeft = useTransform(scrollYProgress, [0, 0.5], [0, -2000])
 
   const handleClick = (() => {
     setIsDialogOpen(true);
@@ -45,21 +48,46 @@ export default function Home() {
       </Dialog>
       <TitleCardAndPlot image={opening} paragraph={plot} />
       <Scene1 />
-      <div ref={ref} className="h-[500vh] bg-black mt-28">
-        <div className="sticky top-0 h-[100vh]">
+      <div ref={ref} className="h-[400vh] bg-black mt-28">
+        <div className="sticky top-0 h-[200vh]">
           <motion.div className="relative">
-            <motion.div style={{ scale: zoom, opacity, transformOrigin: '50% 50%' }}>
+            <motion.div style={{ scale: zoom, opacity, transformOrigin: '70% 70%' }}>
               <Image
                 src={map}
                 alt="map"
                 className="w-full h-full" />
             </motion.div>
-            <motion.div className="absolute inset-0 flex items-center justify-center bg-red-400" style={{ scale: revOpacity }}>
-              <Image
-                src={war}
-                alt="war"
-                className="w-full" />
+            <motion.div
+              ref={warRef}
+              className="absolute bg-red-400 inset-0 overflow-hidden "
+              style={{ scale: revOpacity }}
+            >
+              <div className="relative w-[200vh] h-full">
+              <motion.div
+                className="absolute flex flex-row w-[200vh] h-full bg-red-400"
+                style={{x: moveLeft}}
+              >
+                <Image
+                  src={war}
+                  alt="war"
+                  className="w-full h-[95%]"
+                />
+                <Image
+                  src={war}
+                  alt="war"
+                  className="w-full h-[95%]"
+                />
+                <Image
+                  src={war}
+                  alt="war"
+                  className="w-full h-[95%]"
+                />
+              </motion.div>
+                <p className="absolute font-edu-hand text-lg flex justify-center w-[90vh] inset-[50vh]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta esse aliquid vitae quam, ex, ipsum ratione, nostrum illo voluptatibus commodi saepe sint mollitia vero tenetur ut quod animi tempore.</p>
+              </div>
             </motion.div>
+            
+
           </motion.div>
         </div>
       </div>
