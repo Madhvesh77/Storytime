625721e8b5b874e982801df90332f4c562406cc1
Madhvesh77 <madhvesh77@gmail.com>
2024-08-11 17:48:17 +0530

    replace + with map image

--- storytime/src/app/page.tsx
+++ storytime/src/app/page.tsx
@@ -9,7 +9,7 @@ import black from "../../public/BlackBase.png";
 import leftHill from "../../public/Hill4Black.png";
 import rightHill from "../../public/Hill5Black.png";
 import leaf from "../../public/LeafBlack.png";
-import map from "../../public/Map.png";
+import map from "../../public/Map.jpg";
 import opening from "../../public/Opening.png";
 import sun from "../../public/SunsetBg.jpeg";
 import children from "../../public/SunsetChildren.png";
@@ -31,9 +31,11 @@ export default function Home() {
       <Button
         visible={!isDialogOpen}
         onClick={handleClick}
-        className="fixed ml-[96%] mt-[55%] z-50 w-12 h-12 bg-blue-500 text-white rounded-full shadow-lg flex items-center justify-center"
+        className="fixed ml-[96%] mt-[55%] z-50 w-12 h-12 bg-transparent text-white rounded-full shadow-lg flex items-center justify-center"
       >
-        +
+        <Image 
+        src={map}
+        alt="map-icon"/>
       </Button>
       <Dialog header="" visible={isDialogOpen} style={{ width: '50vw' }} onHide={() => setIsDialogOpen(false)}>
         <Image
