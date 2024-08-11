6336a417e453b15cf41c4eb28faaaa087d970bb4
Madhvesh77 <madhvesh77@gmail.com>
2024-08-11 23:10:11 +0530

    add opacity to text in scene 2

--- storytime/src/components/Chapter1/Scene2/index.tsx
+++ storytime/src/components/Chapter1/Scene2/index.tsx
@@ -5,13 +5,12 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 export default function Scene2() {
     const ref = useRef(null);
-    const warRef = useRef(null);
-    const { scrollYProgress: warScrollYProgress } = useScroll({ target: warRef })
     const { scrollYProgress } = useScroll({ target: ref });
     const zoom = useTransform(scrollYProgress, [0, 0.2], [1, 4]);
     const opacity = useTransform(scrollYProgress, [0, 0.2], [1, 0]);
     const revOpacity = useTransform(scrollYProgress, [0.1, 0.3], [0, 1]);
     const moveLeft = useTransform(scrollYProgress, [0, 0.5], [0, -2000])
+    const textOpacity = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
     return (
         <div ref={ref} className="h-[400vh] bg-black mt-28">
             <div className="sticky top-0 h-[200vh]">
@@ -23,7 +22,6 @@ export default function Scene2() {
                             className="w-full h-full" />
                     </motion.div>
                     <motion.div
-                        ref={warRef}
                         className="absolute bg-red-400 inset-0 overflow-hidden "
                         style={{ scale: revOpacity }}
                     >
@@ -48,7 +46,9 @@ export default function Scene2() {
                                     className="w-full h-[95%]"
                                 />
                             </motion.div>
-                            <p className="absolute font-edu-hand text-lg flex justify-center w-[90vh] inset-[50vh]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta esse aliquid vitae quam, ex, ipsum ratione, nostrum illo voluptatibus commodi saepe sint mollitia vero tenetur ut quod animi tempore.</p>
+                            <motion.div className="absolute flex justify-center w-[90vh] inset-[50vh]" style={{opacity: textOpacity}}>
+                            <p className="font-edu-hand text-lg ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem soluta esse aliquid vitae quam, ex, ipsum ratione, nostrum illo voluptatibus commodi saepe sint mollitia vero tenetur ut quod animi tempore.</p>
+                            </motion.div>
                         </div>
                     </motion.div>
 
