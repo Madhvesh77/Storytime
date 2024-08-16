a90d65010198c1a7a2797365c0f4572f03b49c73
Madhvesh77 <madhvesh77@gmail.com>
2024-08-16 23:49:46 +0530

    extract scene4 as separate component

--- storytime/src/app/page.tsx
+++ storytime/src/app/page.tsx
@@ -3,14 +3,13 @@ import TitleCardAndPlot from "@/components/TitleCardAndPlot";
 import Image from "next/image";
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
-import { useRef, useState } from "react";
+import { useState } from "react";
 import map from "../../public/Map.png";
-import girl from "../../public/GirlBlur.png";
 import opening from "../../public/Opening.png";
 import Scene1 from "@/components/Chapter1/Scene1";
 import Scene2 from "@/components/Chapter1/Scene2";
 import Scene3 from "@/components/Chapter1/Scene3";
-import { motion, useScroll, useTransform } from "framer-motion";
+import Scene4 from "@/components/Chapter1/Scene4";
 
 export default function Home() {
   const plot = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi rem sed commodi cumque accusamus molestiae, sequi ullam accusantium modi, facere tenetur fuga eos cum nisi maiores, ad saepe aspernatur! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, aliquam. Libero excepturi eaque, eius velit est repellendus fugiat dolores voluptatem quia, adipisci, officiis ad cupiditate blanditiis nostrum aut delectus illum?"
@@ -19,11 +18,6 @@ export default function Home() {
   const handleClick = (() => {
     setIsDialogOpen(true);
   })
-
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({ target: ref });
-  const opacity = useTransform(scrollYProgress, [0.3, 0.7, 1], [0, 0.5, 0]);
-  const textOpacity = useTransform(scrollYProgress, [0.3, 0.7, 1], [0, 2, 0]);
 
   return (
     <main className=" mb-[100vh]">
@@ -49,42 +43,7 @@ export default function Home() {
       <Scene1 />
       <Scene2 />
       <Scene3 />
-
-      <div ref={ref} className="bg-black h-[300vh] mt-5">
-        <div className="flex justify-center items-center flex-col gap-5 h-[20%]">
-          <p className="max-w-[75%] font-edu-hand text-3xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat in assumenda minima, laudantium quibusdam tempore
-          </p>
-          <p className="max-w-[75%] font-edu-hand text-4xl font-extrabold">
-            Lorem ipsum dolor sit amet consectetur
-          </p>
-        </div>
-        <motion.div className="sticky top-0 h-[100vh] bg-black">
-          <div className="h-full w-full relative">
-            <motion.div
-              className="absolute inset-0"
-              style={{ opacity: opacity }}
-            >
-              <Image
-                src={girl}
-                alt="girl"
-                className="w-full h-full object-cover"
-              />
-            </motion.div>
-            <motion.div className="absolute inset-0 flex justify-center items-center" style={{ opacity: textOpacity }}>
-              <p className="font-edu-hand text-white text-lg max-w-[75%]">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, provident? Molestiae perspiciatis voluptatibus quibusdam sit, maxime similique! Necessitatibus doloremque cupiditate voluptates. Minus assumenda sit, quis repellendus a vitae est distinctio!
-              </p>
-            </motion.div>
-          </div>
-          <div className="flex justify-center items-center w-full h-full">
-            <p className="font-edu-hand text-lg max-w-[75%]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nobis impedit quia unde cum ad labore quos enim. Laboriosam optio vitae saepe, ad consequatur eos placeat minima ipsum incidunt nam.
-            </p>
-          </div>
-        </motion.div>
-
-      </div>
+      <Scene4 />
     </main>
   )
 }
---
+++ storytime/src/components/Chapter1/Scene4/index.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import girl from "../../../../public/GirlBlur.png";
+
+export default function Scene4() {
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll({ target: ref });
+    const opacity = useTransform(scrollYProgress, [0.3, 0.7, 1], [0, 0.5, 0]);
+    const textOpacity = useTransform(scrollYProgress, [0.3, 0.7, 1], [0, 2, 0]);
+    return (
+        <div ref={ref} className="bg-black h-[300vh] mt-5">
+            <div className="flex justify-center items-center flex-col gap-5 h-[20%]">
+                <p className="max-w-[75%] font-edu-hand text-3xl">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat in assumenda minima, laudantium quibusdam tempore
+                </p>
+                <p className="max-w-[75%] font-edu-hand text-4xl font-extrabold">
+                    Lorem ipsum dolor sit amet consectetur
+                </p>
+            </div>
+            <motion.div className="sticky top-0 h-[100vh] bg-black">
+                <div className="h-full w-full relative">
+                    <motion.div
+                        className="absolute inset-0"
+                        style={{ opacity: opacity }}
+                    >
+                        <Image
+                            src={girl}
+                            alt="girl"
+                            className="w-full h-full object-cover"
+                        />
+                    </motion.div>
+                    <motion.div className="absolute inset-0 flex justify-center items-center" style={{ opacity: textOpacity }}>
+                        <p className="font-edu-hand text-white text-lg max-w-[75%]">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, provident? Molestiae perspiciatis voluptatibus quibusdam sit, maxime similique! Necessitatibus doloremque cupiditate voluptates. Minus assumenda sit, quis repellendus a vitae est distinctio!
+                        </p>
+                    </motion.div>
+                </div>
+                <div className="flex justify-center items-center w-full h-full">
+                    <p className="font-edu-hand text-lg max-w-[75%]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nobis impedit quia unde cum ad labore quos enim. Laboriosam optio vitae saepe, ad consequatur eos placeat minima ipsum incidunt nam.
+                    </p>
+                </div>
+            </motion.div>
+        </div>
+    )
+}
