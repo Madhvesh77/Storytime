a2f228cc306a5b5aa4f52239379284fa10d322cc
Madhvesh77 <madhvesh77@gmail.com>
2024-08-16 21:33:52 +0530

    Extract scene3 as separate component

--- storytime/src/app/page.tsx
+++ storytime/src/app/page.tsx
@@ -3,14 +3,12 @@ import TitleCardAndPlot from "@/components/TitleCardAndPlot";
 import Image from "next/image";
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
-import { useRef, useState } from "react";
+import { useState } from "react";
 import map from "../../public/Map.png";
 import opening from "../../public/Opening.png";
-import water from "../../public/water.jpg";
-import boy from "../../public/Boy.png";
 import Scene1 from "@/components/Chapter1/Scene1";
 import Scene2 from "@/components/Chapter1/Scene2";
-import { motion, useScroll, useTransform } from "framer-motion";
+import Scene3 from "@/components/Chapter1/Scene3";
 
 export default function Home() {
   const plot = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quasi rem sed commodi cumque accusamus molestiae, sequi ullam accusantium modi, facere tenetur fuga eos cum nisi maiores, ad saepe aspernatur! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, aliquam. Libero excepturi eaque, eius velit est repellendus fugiat dolores voluptatem quia, adipisci, officiis ad cupiditate blanditiis nostrum aut delectus illum?"
@@ -20,30 +18,6 @@ export default function Home() {
     setIsDialogOpen(true);
   })
 
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({ target: ref });
-  const opacity = useTransform(scrollYProgress, [0.6, 0.7, 1], [0, 2, 0]);
-  let boyDrownAnimation;
-  if (typeof window !== "undefined") {
-    boyDrownAnimation = useTransform(scrollYProgress, [0.8, 0.9], [0, window.innerWidth / 2]);
-  }
-  const [rotation, setRotation] = useState({ x: 0, y: 0 });
-  const handleMouseMove = (event: any) => {
-    const { clientX: cursorX, clientY: cursorY, currentTarget } = event;
-    const { width, height, left, top } = currentTarget.getBoundingClientRect();
-
-    const offsetX = (cursorX - left) - width / 2;
-    const offsetY = (cursorY - top) - height / 2;
-
-    const tiltX = (offsetY / height) * -10;
-    const tiltY = (offsetX / width) * 10;
-
-    setRotation({ x: tiltX, y: tiltY });
-  };
-
-  const handleMouseLeave = () => {
-    setRotation({ x: 0, y: 0 });
-  };
   return (
     <main className=" mb-[100vh]">
       <Button
@@ -67,51 +41,7 @@ export default function Home() {
       <TitleCardAndPlot image={opening} paragraph={plot} />
       <Scene1 />
       <Scene2 />
-      <div className="bg-[#165e6e] top-0 h-screen w-screen">
-        <motion.div
-          className="relative w-full h-full"
-          onMouseMove={handleMouseMove}
-          onMouseLeave={handleMouseLeave}
-          style={{
-            perspective: 1000,
-            opacity: opacity,
-          }}
-        >
-          <div className="absolute inset-0">
-            <Image
-              src={water}
-              alt="water"
-              className="w-full h-full"
-            />
-          </div>
-          <motion.div
-            style={{
-              rotateX: rotation.x,
-              rotateY: rotation.y,
-            }}
-            transition={{ type: "spring", stiffness: 300, damping: 30 }}
-            className="w-full h-full"
-          >
-            <Image
-              src={water}
-              alt="water"
-              className="w-full h-full"
-            />
-          </motion.div>
-          <motion.div className="absolute inset-0 flex justify-center items-start" style={{ y: boyDrownAnimation }}>
-            <Image
-              src={boy}
-              alt="boy"
-              className="w-auto h-[20vh]"
-            />
-          </motion.div>
-          <motion.div className="absolute inset-0 ml-[10%] mr-[10%] flex items-center" style={{ opacity: opacity }}>
-            <p className="w-full flex justify-center items-center text-lg font-edu-hand">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quisquam impedit officiis, iure maiores explicabo quasi dolore sint quidem minima cum neque doloremque corporis fuga blanditiis, adipisci praesentium numquam atque.
-            </p>
-          </motion.div>
-        </motion.div>
-      </div>
+      <Scene3 />
     </main>
   )
 }
---
+++ storytime/src/components/Chapter1/Scene3/index.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import water from "../../../../public/water.jpg";
+import boy from "../../../../public/Boy.png"
+export default function Scene3() {
+    const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const opacity = useTransform(scrollYProgress, [0.6, 0.7, 1], [0, 2, 0]);
+  let boyDrownAnimation;
+  if (typeof window !== "undefined") {
+    boyDrownAnimation = useTransform(scrollYProgress, [0.8, 0.9], [0, window.innerWidth / 2]);
+  }
+  const [rotation, setRotation] = useState({ x: 0, y: 0 });
+  const handleMouseMove = (event: any) => {
+    const { clientX: cursorX, clientY: cursorY, currentTarget } = event;
+    const { width, height, left, top } = currentTarget.getBoundingClientRect();
+
+    const offsetX = (cursorX - left) - width / 2;
+    const offsetY = (cursorY - top) - height / 2;
+
+    const tiltX = (offsetY / height) * -10;
+    const tiltY = (offsetX / width) * 10;
+
+    setRotation({ x: tiltX, y: tiltY });
+  };
+
+  const handleMouseLeave = () => {
+    setRotation({ x: 0, y: 0 });
+  };
+    return (
+        <div className="bg-[#165e6e] top-0 h-screen w-screen">
+        <motion.div
+          className="relative w-full h-full"
+          onMouseMove={handleMouseMove}
+          onMouseLeave={handleMouseLeave}
+          style={{
+            perspective: 1000,
+            opacity: opacity,
+          }}
+        >
+          <div className="absolute inset-0">
+            <Image
+              src={water}
+              alt="water"
+              className="w-full h-full"
+            />
+          </div>
+          <motion.div
+            style={{
+              rotateX: rotation.x,
+              rotateY: rotation.y,
+            }}
+            transition={{ type: "spring", stiffness: 300, damping: 30 }}
+            className="w-full h-full"
+          >
+            <Image
+              src={water}
+              alt="water"
+              className="w-full h-full"
+            />
+          </motion.div>
+          <motion.div className="absolute inset-0 flex justify-center items-start" style={{ y: boyDrownAnimation }}>
+            <Image
+              src={boy}
+              alt="boy"
+              className="w-auto h-[20vh]"
+            />
+          </motion.div>
+          <motion.div className="absolute inset-0 ml-[10%] mr-[10%] flex items-center" style={{ opacity: opacity }}>
+            <p className="w-full flex justify-center items-center text-lg font-edu-hand">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quisquam impedit officiis, iure maiores explicabo quasi dolore sint quidem minima cum neque doloremque corporis fuga blanditiis, adipisci praesentium numquam atque.
+            </p>
+          </motion.div>
+        </motion.div>
+      </div>
+    )
+}
