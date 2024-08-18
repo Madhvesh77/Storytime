f25e0069c54043596a7d60654da2644e9e11b5ce
Madhvesh77 <madhvesh77@gmail.com>
2024-08-18 11:12:23 +0530

    take the nextPage as prop to next chapter button

--- storytime/src/app/page.tsx
+++ storytime/src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         </div>
       </Dialog>
       <Chapter1 />
-      <NextChapterButton />
+      <NextChapterButton nextPage="Chapter2"/>
     </main>
   )
 }
--- storytime/src/components/NextChapterButton/index.tsx
+++ storytime/src/components/NextChapterButton/index.tsx
@@ -3,11 +3,11 @@ import next from "../../../public/logo192.png";
 import { Button } from "primereact/button";
 import { useRouter } from "next/navigation";
 
-export default function NextChapterButton() {
+export default function NextChapterButton({nextPage} : {nextPage: string}) {
     const router = useRouter();
     return (
         <div className="w-full flex justify-center items-center pb-10">
-            <Button className="bg-black hover:bg-gray-800 rounded-full" onClick={(() => router.push("Chapter2"))}>
+            <Button className="bg-black hover:bg-gray-800 rounded-full" onClick={(() => router.push(nextPage))}>
                 <Image alt="logo" src={next} className="h-10 w-10 hover:rotate-90" />
             </Button>
         </div>
