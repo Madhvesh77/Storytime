e2ab69cdb73b32ce90000ad2abc83893cfc8514c
Madhvesh77 <madhvesh77@gmail.com>
2024-09-08 15:04:36 +0530

    implement eye opening animation

--- storytime/src/app/(pages)/Chapter2/page.tsx
+++ storytime/src/app/(pages)/Chapter2/page.tsx
@@ -1,7 +1,34 @@
+'use client'
+import { useScroll, useTransform, motion } from 'framer-motion';
+import Head from 'next/head';
+import { useRef } from 'react';
+import girl from "../../../../public/Chapter1/Scene4/GirlBlur.png";
+import Image from "next/image";
 export default function Chapter2() {
-    return (
-        <>
-        <h1>Hello world!</h1>
-        </>
-    )
+    const containerRef = useRef(null);
+    const { scrollYProgress } = useScroll({target: containerRef} );
+    const opacity = useTransform(scrollYProgress, [0, 0.2, 1], [0, 0.5, 0]);
+    const textOpacity = useTransform(scrollYProgress, [0, 0.2, 1], [0, 2, 0]);
+    const finalTextOpacity = useTransform(scrollYProgress, [0.9, 1], [0, 1]);
+    // const upperLidY = useTransform(scrollYProgress, [0, 0.2], [0, 0]);
+    // const lowerLidY = useTransform(scrollYProgress, [0, 0.2], [0, 20]);
+    const helloOpacity = useTransform(scrollYProgress, [0.15, 0.2], [0, 1]);
+    const clipPath = useTransform(
+        scrollYProgress,
+        [0, 0.2, 1],
+        [
+          'inset(50% 0 50% 0)', // Fully closed (lids are touching in the middle)
+          'inset(20% 0 20% 0)', // Fully open (content is fully revealed)
+          'inset(40% 0 40% 0)'
+        ]
+      );
+
+  // Animating scaleY to open the eye as you scroll
+  const scaleY = useTransform(scrollYProgress, [0, 1], [0.05, 1]);
+
+  return (
+    <>
+        <h1>Hello</h1>
+    </>
+  );
 }
--- storytime/src/components/Chapter1/Scene4/index.tsx
+++ storytime/src/components/Chapter1/Scene4/index.tsx
@@ -9,40 +9,81 @@ export default function Scene4() {
     const opacity = useTransform(scrollYProgress, [0.3, 0.7, 1], [0, 0.5, 0]);
     const textOpacity = useTransform(scrollYProgress, [0.3, 0.7, 1], [0, 2, 0]);
     const finalTextOpacity = useTransform(scrollYProgress, [0.9, 1], [0, 1]);
+    const clipPath = useTransform(
+        scrollYProgress,
+        [0, 0.7, 1],
+        [
+            'inset(50% 0 50% 0)',
+            'inset(20% 0 20% 0)',
+            'inset(40% 0 40% 0)',
+        ]
+      );
     return (
-        <div ref={ref} className="bg-black h-[300vh] mt-5">
+        <>
             <div className="flex justify-center items-center flex-col gap-5 h-[20%]">
-                <p className="max-w-[75%] font-edu-hand text-3xl">
+                <p className="max-w-[75%] font-edu-hand text-3xl mt-72">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat in assumenda minima, laudantium quibusdam tempore
                 </p>
                 <p className="max-w-[75%] font-edu-hand text-4xl font-extrabold">
                     Lorem ipsum dolor sit amet consectetur
                 </p>
             </div>
-            <motion.div className="sticky top-0 h-[100vh] bg-black">
-                <div className="h-full w-full relative">
-                    <motion.div
-                        className="absolute inset-0"
-                        style={{ opacity: opacity }}
-                    >
-                        <Image
-                            src={girl}
-                            alt="girl"
-                            className="w-full h-full object-cover"
-                        />
-                    </motion.div>
-                    <motion.div className="absolute inset-0 flex justify-center items-center" style={{ opacity: textOpacity }}>
-                        <p className="font-edu-hand text-white text-lg max-w-[75%]">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, provident? Molestiae perspiciatis voluptatibus quibusdam sit, maxime similique! Necessitatibus doloremque cupiditate voluptates. Minus assumenda sit, quis repellendus a vitae est distinctio!
-                        </p>
-                    </motion.div>
-                    <motion.div className="flex justify-center items-center w-full h-full" style={{opacity: finalTextOpacity}}>
-                        <p className="font-edu-hand text-lg max-w-[75%]">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nobis impedit quia unde cum ad labore quos enim. Laboriosam optio vitae saepe, ad consequatur eos placeat minima ipsum incidunt nam.
-                        </p>
-                    </motion.div>
-                </div>
-            </motion.div>
+            <div ref={ref} className="h-[300vh] bg-gray-900">
+        <div className="sticky top-0 h-screen w-full flex items-center justify-center bg-black overflow-hidden">
+          {/* Upper lid */}
+          <motion.div
+            className="absolute top-0 left-0 w-full h-1/2 bg-black"
+            // style={{ translateY: upperLidY }}
+          />
+
+          {/* Lower lid */}
+          <motion.div
+            className="absolute bottom-0 left-0 w-full h-1/2 bg-black"
+            // style={{ translateY: lowerLidY }}
+          />
+
+          {/* Content to be revealed between the eyelids */}
+          <motion.div
+            className="absolute h-full w-full"
+            style={{ clipPath }}
+          >
+            <div className="h-full w-full relative">
+              {/* Image */}
+              <motion.div
+                className="absolute inset-0"
+                style={{ opacity }}
+              >
+                <Image
+                  src={girl}
+                  alt="girl"
+                  className="w-full h-full object-cover"
+                />
+              </motion.div>
+
+              {/* First text content */}
+              <motion.div
+                className="absolute inset-0 flex justify-center items-center"
+                style={{ opacity: textOpacity }}
+              >
+                <p className="font-edu-hand text-white text-lg max-w-[75%]">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, provident? Molestiae perspiciatis voluptatibus quibusdam sit, maxime similique! Necessitatibus doloremque cupiditate voluptates. Minus assumenda sit, quis repellendus a vitae est distinctio!
+                </p>
+              </motion.div>
+
+              {/* Final text content */}
+              <motion.div
+                className="absolute flex justify-center items-center w-full h-full"
+                style={{ opacity: finalTextOpacity }}
+              >
+                <p className="font-edu-hand text-lg max-w-[75%]">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nobis impedit quia unde cum ad labore quos enim. Laboriosam optio vitae saepe, ad consequatur eos placeat minima ipsum incidunt nam.
+                </p>
+              </motion.div>
+            </div>
+          </motion.div>
         </div>
+      </div>
+
+        </>
     )
 }
